ef87c5657aa07b60a891a1501a677ed5e7ec917c
Erik Ostrom <erik@echographia.com>
2010-08-08 15:51:48 -0700

    Fixed test_helper path in test files.
    This should have been done in a5efd44b (moved Hobo to a subsite), but
    I wasn't running tests yet so I didn't notice.

--- test/functional/admin/books_controller_test.rb
+++ test/functional/admin/books_controller_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
 class Admin::BooksControllerTest < ActionController::TestCase
   # Replace this with your real tests.
