b1bd73761b16751d2fc61ab71efc87f75a71f276
Erik Ostrom <erik@echographia.com>
2010-08-15 01:32:51 -0700

    Made sure page routes are read-only.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.site_search  'search', :controller => 'front', :action => 'search'
   map.root :controller => 'pages', :action => 'show'
-  map.resources :pages
+  map.resources :pages, :only => [:index, :show]
 
   Hobo.add_routes(map)
 
