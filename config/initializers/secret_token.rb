dd573952493d67a710b49205a771353998a1078a
Erik Ostrom <erik@echographia.com>
2010-10-09 22:37:38 -0700

    Rails 3: Updated cookie verification secret token configuration.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '0f92c76873f1aadd68a03e4e0cc9488fcf1b6c3a47d00479a92c98873e9db467011639972f9ad54a9b46d822b8a28c636d10929509ba7967104e414af3ee652e';
+Comics::Application.config.secret_token = '796b762cea050c09aa86d6423750adc52a0ef192bbafbd7a76d846a0956040a28207b6399a601720e27fa2e457a0e3d904c679ce3d7b442f0de3e2c153f835cd'
