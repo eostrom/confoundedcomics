7723f3a2be70ce0cde26ba9302ba5a6099abddb4
Erik Ostrom <erik@echographia.com>
2011-01-19 18:15:13 -0600

    "Transparent messages"-like flash messages.
    Messages stay visible until you move the mouse, then slowly fade.
    Mousing over the message stops and reverses the fade. Clicking
    elsewhere on the page speeds the fade.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,4 +21,33 @@ jQuery(function($) {
     $('#archive #pages').show();
     return false;
   });
+
+  (function() {
+    var flashes = $('.flashes').addClass('dismissable');
+
+    var fadeout = function(timeout) {
+      return function() {
+        flashes.stop(true).fadeOut(timeout, function() {
+          $('body')
+            .unbind('mousemove', slowfade)
+            .unbind('click', fastfade)
+          ;
+        });
+      };
+    }
+
+    var slowfade = fadeout(3000), fastfade = fadeout(200);
+    var stopevent = function() { return false; };
+
+    $('body')
+      .mousemove(slowfade)
+      .click(fastfade);
+
+    flashes
+      .mousemove(stopevent)
+      .click(stopevent)
+      .mouseover(function() {
+        $(this).stop(true).animate({opacity: 1}, {duration: 2000});
+      });
+  })();
 });
